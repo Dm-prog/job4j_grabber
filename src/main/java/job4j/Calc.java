685d8e642b04f64b9fb35da6438f496620f830f8
Dm-prog <prostokota@list.ru>
2021-02-04 18:01:40 +0300

    Test

---
+++ src/main/java/job4j/Calc.java
@@ -0,0 +1,8 @@
+package job4j;
+
+public class Calc {
+
+    public int res(int x, int y) {
+        return x + y;
+    }
+}
---
+++ src/test/java/job4j/CalcTest.java
@@ -0,0 +1,16 @@
+package job4j;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CalcTest {
+
+    Calc calc = new Calc();
+
+    @Test
+    public void resSum() {
+        assertThat(5, is(calc.res(2, 3)));
+    }
+}
