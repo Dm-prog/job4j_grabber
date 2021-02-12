e3452c41643e2f3d8c32a028f5cc358f1630286f
Dm-prog <prostokota@list.ru>
2021-02-12 12:54:39 +0300

    1. Quartz[175122#406250]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,48 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        try (InputStream rabbitProperties =
+                     AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")
+        ) {
+            config.load(rabbitProperties);
+            //Управление таймерами запуска задач.
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            //Создание задачи.
+            JobDetail job = newJob(Rabbit.class).build();
+            //Создание расписания.
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(10)
+                    .repeatForever();
+            //Задача выполняется через триггер.
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            //Загрузка задачи и триггера в планировщик.
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException | IOException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
