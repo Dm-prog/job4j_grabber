37ae966e0a33c3fe9707c96960afef66e1330abd
Dm-prog <prostokota@list.ru>
2021-02-16 16:24:28 +0300

    1.1. Job c параметрами[260360#406254]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,11 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -18,31 +23,50 @@ public class AlertRabbit {
                      AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")
         ) {
             config.load(rabbitProperties);
-            //Управление таймерами запуска задач.
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            //Создание задачи.
-            JobDetail job = newJob(Rabbit.class).build();
-            //Создание расписания.
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
-                    .repeatForever();
-            //Задача выполняется через триггер.
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            //Загрузка задачи и триггера в планировщик.
-            scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException | IOException se) {
+            //Подклюение к базе
+            Class.forName(config.getProperty("driver"));
+            try (Connection connection = DriverManager.getConnection(
+                config.getProperty("url"),
+                config.getProperty("postgres"),
+                config.getProperty("password")
+            )) {
+                //Управление таймерами запуска задач.
+                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+                JobDataMap data = new JobDataMap();
+                data.put("connection", connection);
+                //Создание задачи.
+                JobDetail job = newJob(Rabbit.class).build();
+                //Создание расписания.
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
+                        .repeatForever();
+                //Задача выполняется через триггер.
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                //Загрузка задачи и триггера в планировщик.
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            }
+        } catch (SchedulerException | IOException | ClassNotFoundException
+                | SQLException | InterruptedException se) {
             se.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
+            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
+            store.add(System.currentTimeMillis());
         }
     }
 }
