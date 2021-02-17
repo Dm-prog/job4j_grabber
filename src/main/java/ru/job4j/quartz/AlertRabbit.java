728290517d86c391b5fca3d48a411a1dc9fa7273
Dm-prog <prostokota@list.ru>
2021-02-17 14:16:11 +0300

    1.1. Job c параметрами[260360#406254]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,58 +3,54 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Properties;
 
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Properties config = new Properties();
         try (InputStream rabbitProperties =
                      AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")
         ) {
             config.load(rabbitProperties);
-            //Подклюение к базе
-            Class.forName(config.getProperty("driver"));
-            try (Connection connection = DriverManager.getConnection(
+        }
+        Class.forName(config.getProperty("driver"));
+        //Подклюение к базе
+        try (Connection connection = DriverManager.getConnection(
                 config.getProperty("url"),
                 config.getProperty("postgres"),
                 config.getProperty("password")
-            )) {
-                //Управление таймерами запуска задач.
-                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-                scheduler.start();
-                JobDataMap data = new JobDataMap();
-                data.put("connection", connection);
-                //Создание задачи.
-                JobDetail job = newJob(Rabbit.class).build();
-                //Создание расписания.
-                SimpleScheduleBuilder times = simpleSchedule()
-                        .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
-                        .repeatForever();
-                //Задача выполняется через триггер.
-                Trigger trigger = newTrigger()
-                        .startNow()
-                        .withSchedule(times)
-                        .build();
-                //Загрузка задачи и триггера в планировщик.
-                scheduler.scheduleJob(job, trigger);
-                Thread.sleep(10000);
-                scheduler.shutdown();
-            }
-        } catch (SchedulerException | IOException | ClassNotFoundException
-                | SQLException | InterruptedException se) {
-            se.printStackTrace();
+        )) {
+            //Управление таймерами запуска задач.
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", connection);
+            //Создание задачи.
+            JobDetail job = newJob(Rabbit.class).build();
+            //Создание расписания.
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
+                    .repeatForever();
+            //Задача выполняется через триггер.
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            //Загрузка задачи и триггера в планировщик.
+            scheduler.scheduleJob(job, trigger);
+            Thread.sleep(10000);
+            scheduler.shutdown();
         }
+
     }
 
     public static class Rabbit implements Job {
@@ -63,10 +59,15 @@ public class AlertRabbit {
         }
 
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
-            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
-            store.add(System.currentTimeMillis());
+        public void execute(JobExecutionContext context) {
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            PreparedStatement preparedStatement;
+            try {
+                preparedStatement = connection.prepareStatement("insert into rabbit (created_date) values (?)");
+                preparedStatement.setLong(1, System.currentTimeMillis());
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
     }
 }
