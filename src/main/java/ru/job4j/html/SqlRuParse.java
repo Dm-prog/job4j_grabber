2d5c4fe7ad0184871d6ac1d2708232f95ea3bcbe
Dm-prog <prostokota@list.ru>
2021-02-19 14:20:56 +0300

    2. Парсинг HTML страницы.[260358#406252]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,18 +7,21 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
+        // Получаем HTML страницу
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        // Находим данные по критерию поиска ".postslisttopic"
         Elements row = doc.select(".postslisttopic");
+        // Разбиваем по строкам
         for (Element td : row) {
+           //Получаем сспервый элемент строки
             Element href = td.child(0);
+            // Поучаем ссылку на строку
             System.out.println(href.attr("href"));
+            //Получаем текст строки
             System.out.println(href.text());
-        }
-        Elements date = doc.select(".altCol");
-        for (Element td : date) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
+            //Получаем 6-ой элемент от...
+            Element data = td.parent().child(5);
+            System.out.println(data.text());
         }
     }
 }
