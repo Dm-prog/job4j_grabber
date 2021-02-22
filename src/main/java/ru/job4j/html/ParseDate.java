2b8bb91de2956ba6c776a8e30bd39482feac47de
Dm-prog <prostokota@list.ru>
2021-02-22 11:55:05 +0300

    2.1. Преобразование даты[289476#406260]

--- src/main/java/ru/job4j/html/ParseDate.java
+++ src/main/java/ru/job4j/html/ParseDate.java
@@ -11,29 +11,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class SqlRuParse {
+public class ParseDate {
     public static SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy, HH:mm", Locale.US);
-
-    public static void main(String[] args) throws Exception {
-        parsePage();
-    }
-
-    public static void parsePage() throws IOException {
-        // Получаем HTML страницу
+    public void parse() throws IOException, ParseException {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        // Находим данные по критерию поиска ".postslisttopic"
         Elements row = doc.select(".postslisttopic");
-        // Разбиваем по строкам
         for (Element td : row) {
-            //Получаем сспервый элемент строки
             Element href = td.child(0);
-            // Поучаем ссылку на строку
             System.out.println(href.attr("href"));
-            //Получаем текст строки
             System.out.println(href.text());
-            //Получаем 6-ой элемент от...
+
             Element data = td.parent().child(5);
             System.out.println(data.text());
+            String dateInString = data.text();
+            dateFormat.applyPattern(dateInString);
+            Date date = dateFormat.parse(dateInString);
+            System.out.println(dateFormat.format(date));
         }
     }
 }
