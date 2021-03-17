7d927944810f1fe177c68b1a429daa3c84cb22e0
Dm-prog <prostokota@list.ru>
2021-03-17 13:52:33 +0300

    2.2. Модель данных - Post.[285211#406257]

--- db/post.sql
+++ db/post.sql
@@ -1,7 +1,6 @@
 create table post (
     id serial primary key,
     title varchar(255) not null,
-    author varchar(255) not null,
     date timestamp,
     description text not null,
     link varchar(255) not null
