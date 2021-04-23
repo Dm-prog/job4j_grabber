7c7c39bb0522edb4de51cb73816192d0093f1d23
Dm-prog <prostokota@list.ru>
2021-04-23 19:21:11 +0300

    6. Grabber.[289477#406261]

---
+++ db/select_by_company.sql
@@ -0,0 +1,24 @@
+CREATE TABLE company
+(
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer,
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+-- имена всех person, которые не состоят в компании с id = 5 и
+-- название компании для каждого человека.
+select p.name, c.name from person p join company c on c.id = p.company_id and p.company_id != 5;
+
+-- Необходимо выбрать название компании с максимальным количеством
+-- человек + количество человек в этой компании.
+select c.name, (select sum(count(p.company_id))) as max_count_people_in_company
+from person p join company c on c.id = p.company_id GROUP BY c.name order by count(*) desc
+limit 1;
