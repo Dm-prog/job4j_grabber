8444d88fb644e665cdb1e39ff9683dfb7274b0b5
Dm-prog <prostokota@list.ru>
2021-05-17 00:22:35 +0300

    SQL [#224183]

--- db/SQL [#224183].sql
+++ db/SQL [#224183].sql
@@ -2,14 +2,14 @@
 create table meetings
 (
     id serial primary key,
-    name varchar(255)
+    name varchar(255),
+    request boolean
 );
 
 create table users
 (
     id serial primary key,
-    name varchar(255),
-    request boolean
+    name varchar(255)
 );
 
 create table users_meetings
@@ -17,19 +17,19 @@ create table users_meetings
     user_id integer not null,
     meeting_id integer not null,
     foreign key(user_id) references users(id),
-    foreign key(meeting_id) references users(id)
+    foreign key(meeting_id) references meetings(id)
 );
 
-insert into users (Name, request) values
-('Ivan', true),
-('Andrey', true),
-('Evgeniy', true),
-('Boris', false);
+insert into users (name) values
+('Ivan'),
+('Andrey'),
+('Evgeniy'),
+('Boris');
 
-insert into meetings (Name) values
-('conference'),
-('corporate_events'),
-('business_meeting');
+insert into meetings (name, request) values
+('conference', true),
+('corporate_events', true),
+('business_meeting', false);
 
 insert into users_meetings values
 (1, 1),
@@ -42,11 +42,11 @@ insert into users_meetings values
 -- всех заяков и количество подтвердивших участников.
 select m.name as list_of_meetings, count(*) as count_users from meetings m
 join users_meetings um on m.id = um.meeting_id
-join users u on u.id = um.users_id
-where u.request = true group by m.name
+join users u on u.id = um.user_id
+where m.request = true group by m.name
 
 -- Нужно получить все совещания, где не было ни одной заявки на посещения
 select m.name as meetings_without_users from meetings m
 left join users_meetings um on m.id = um.meeting_id
 left join users u on u.id = um.user_id
-where u.request = false or u.request is null
+where m.request = false or m.request is null
